67d92508001fa490069267ebdc876c12ea61b9db
Tejaswi Konduri <tkonduri@uncc.edu>
2017-02-14 14:38:18 -0500

    Client and server code for SpeedTest assignment.

---
+++ Platform9-Client/Platform9-Client/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Platform9_Client
+{
+    class clientOperations
+    {
+
+        static string path = "../../test.txt";
+        static string operation = "PUT";
+        static int iLength;
+        private static void connectToServerUsingTcp(string host, int port)
+        {
+            try
+            {
+                string szMsg = host + " " + Convert.ToString(port) + " " + operation + " " + path;
+                string szReceived = "";
+
+                using (TcpClient tClient = new TcpClient(host, port))
+                {
+                    Console.WriteLine("Connection was established...");
+
+                    using (NetworkStream nStream = tClient.GetStream())
+                    {
+                        Console.WriteLine("Stream was received from the connection...");
+
+                        ///Sending a message
+                        BinaryWriter bWriter = new BinaryWriter(nStream);
+                        bWriter.Write(szMsg);
+                        Console.WriteLine(szMsg + " was sent...");
+                        
+                        ///Receiving a message and displaying it
+                        BinaryReader bReader = new BinaryReader(nStream);
+                        szReceived = bReader.ReadString();
+
+                        //Measuring the download time...
+                        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                        sendFile(host, port, path, tClient, nStream, bWriter);
+
+                        watch.Stop();
+                        var elapsedMs = watch.ElapsedMilliseconds;
+
+                        Console.WriteLine("\n\n### Upload Speed:" + (iLength / elapsedMs)*1000 + " in Bits / sec");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void sendFile(string ip, int port, string path, TcpClient tClient, NetworkStream nStream, BinaryWriter bWriter)
+        {
+            try
+            {
+                Console.WriteLine("Sending file " + path + " ...");
+                
+                byte[] SendingBuffer = null;
+
+                FileStream Fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                int iBufferSize = 1024;
+                int iNoOfPackets = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(iBufferSize)));
+                int iTotalLength = (int)Fs.Length, iCounter = 0, iCurrentPacketLength = 0, iCtr = 0;
+                iLength = iTotalLength;
+
+                for (iCounter = 0; iCounter < iNoOfPackets; iCounter++)
+                {
+                    if (iBufferSize < iTotalLength)
+                    {
+                        iCurrentPacketLength = iBufferSize;
+                        iTotalLength = iTotalLength - iCurrentPacketLength;
+                    }
+                    else
+                    {
+                        iCurrentPacketLength = iTotalLength;
+                    }
+
+                    SendingBuffer = new byte[iCurrentPacketLength];
+                    Fs.Read(SendingBuffer, 0, iCurrentPacketLength);
+
+                    bWriter = new BinaryWriter(nStream);
+                    bWriter.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+
+                    iCtr += (int)SendingBuffer.Length;
+                    Console.WriteLine(iCtr + " byte were written/sent ...");
+                }
+
+                Fs.Close();                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                ///Fetching the inputs from command line
+                var host = args[0];
+                Console.WriteLine("Host = " + host);
+                
+                int port = 0;
+                int.TryParse(args[1], out port);
+                Console.WriteLine("Port = " + port);
+                
+                ///Setting up connection with the server, and sending requests and receiving response...
+                connectToServerUsingTcp(host, port);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Platform9/Platform9/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Platform9
+{
+    public class ServerMain
+    {
+        static int port;
+        static string szIP = "";
+        //Assumption: We are calculating the test.txt files speed of upload/downloading
+        
+        /// File name that will be saved on the server on the path where the myserver.exe is present
+        const string SaveFileName = "../../DownloadedFile.txt";
+        
+        private static string saveFile(/*int port, */NetworkStream nStream, TcpClient tClient/*, TcpListener listener*/)
+        {
+            string szPUTReply = "200 OK FILE CREATED";
+            int iBufferSize = 1024;
+            
+            try
+            {
+                Console.WriteLine("Saving the file...");
+
+                byte[] RecData = new byte[iBufferSize];
+                int iRecBytes = 0;
+
+                nStream = tClient.GetStream();
+                int iTotalRecBytes = 0;
+
+                //Measuring the download time...
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                ///Saves the file in the current directory of the .exe file
+                FileStream Fs = new FileStream(SaveFileName, FileMode.OpenOrCreate, FileAccess.Write);
+
+                while ((iRecBytes = nStream.Read(RecData, 0, RecData.Length)) > 0)
+                {
+                    Fs.Write(RecData, 0, iRecBytes);
+                    iTotalRecBytes += iRecBytes;
+                }
+
+                Fs.Close();
+
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                //szPUTReply += ", With Time taken = " + elapsedMs + " ms";
+
+                szPUTReply += "\n\n### Download Speed:"+ (iTotalRecBytes/elapsedMs)*1000 +" in Bits / sec";
+
+                Console.WriteLine("File saved successfully...");
+            }
+            catch (Exception ex)
+            {
+                szPUTReply = "204 FILE NOT SAVED";
+                Console.WriteLine("Error while saving the file...Exception occured: " + ex.Message);
+            }
+
+            return szPUTReply;
+        }
+
+        private static void replyRequest(string szReply, NetworkStream nStream)
+        {
+            try
+            {
+                BinaryWriter bWriter = new BinaryWriter(nStream);
+                bWriter.Write(szReply);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+            }
+        }
+
+        private static string handlePUTRequest()
+        {
+            string szPUTReply = "";
+            try
+            {
+                ///For initial confirmation...After receiving this message the client will send the file
+                szPUTReply = "200 OK FILE CREATED";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+                szPUTReply = "204 FILE NOT SAVED";
+            }
+            return szPUTReply;
+        }
+
+        private static int parseRequest(NetworkStream nStream, TcpClient tClient, TcpListener listener)
+        {
+            int iRet = 0;
+            try
+            {                
+                string szPUTReply = "";
+                szPUTReply = handlePUTRequest();
+                replyRequest(szPUTReply, nStream);
+                szPUTReply = saveFile(/*port, */nStream, tClient/*, listener*/);
+                Console.WriteLine(szPUTReply + " was sent...");                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+            }
+
+            return iRet;
+        }
+
+
+        public static void handleRequest()
+        {
+            try
+            {
+                
+                TcpListener listener = new TcpListener(IPAddress.Parse(szIP), port);
+
+                ///Starting the listner
+                listener.Start();
+
+                ///Continous loop
+                while (true)
+                {
+                    Console.WriteLine("\nWaiting for connections...");
+
+                    ///Accept client connection
+                    using (TcpClient tClient = listener.AcceptTcpClient())
+                    {
+                        Console.WriteLine("Connection Request accepted...");
+
+                        using (NetworkStream nStream = tClient.GetStream())
+                        {
+                            BinaryReader bReader = new BinaryReader(nStream);
+                            string szReceived = bReader.ReadString();
+
+                            int iRet = parseRequest(nStream, tClient, listener);                            
+                        }
+
+                        tClient.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+            }
+        }
+
+        public static int Main(String[] args)
+        {
+            try
+            {
+                //ServerMain sMain = null;
+
+                Console.WriteLine("IP address of server: " + args[0] + "\n");
+                szIP = args[0];
+                Console.WriteLine("Port on which server is listening: " + args[1] + "\n");
+                int.TryParse(args[1], out port);
+                
+                ///Multithreaded call to handleRequest function
+                Thread thread = new Thread(handleRequest);
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured: " + ex.Message);
+            }
+            return 0;
+        }
+    }
+}
